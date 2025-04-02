a36b84a1bdbf439e9e0ff4c1b2d56c5ac778c87d
Jonathan Krall <jonathan.krall@lexicon.se>
2025-04-02 13:22:52 +0200

    Added Employee

--- PersonalRegister2025VT/Program.cs
+++ PersonalRegister2025VT/Program.cs
@@ -4,31 +4,16 @@
     {
         static void Main(string[] args)
         {
-            //Droid d1 = new Droid("C-3PO",27000);
-            //Droid d2 = new Droid();
-            //Droid d3 = new Droid("Dalek");
-            //d2.Salary = 2000;
-            //d2.SetFirstName("Potatis"); 
+            Employee emp = new Employee("Anders", 20000);
+            //emp.Name = "Anders";
+            //emp.Salary = 20000;
 
-            //Console.WriteLine(d1.GetFirstName());
-            ////d1.firstname = "R2-D2";
-            //d1.SetFirstName("R2-D2");
-            //Console.WriteLine(d1.GetFirstName());
-            //d1.Salary = 25000;
-            //Console.WriteLine(d1.Salary);
-            //Console.ReadLine();
-            //
-            Droid d1 = new Droid();
-            //d1.Count;
-            Console.WriteLine(Droid.Count);
-            Droid d2 = new Droid();
-            Droid.GetCount(); 
-            Droid d3 = new Droid();
-            Console.WriteLine(Droid.Count);
-            Droid d4 = new Droid();
-            Droid.GetCount();
-            Droid d5 = new Droid();
-            Console.WriteLine(Droid.Count);
+            Console.WriteLine($"Name: {emp.Name} Salary: {emp.Salary}");
+            //Console.WriteLine("Name: " + emp.Name + " Salary: " + emp.Salary);
+            //Console.WriteLine("Name: {0} Salary: {1}", emp.Name, emp.Salary);
+            emp.Name = "Nisse";
+            Console.WriteLine($"Name: {emp.Name} Salary: {emp.Salary}");
+
         }
     }
 }
