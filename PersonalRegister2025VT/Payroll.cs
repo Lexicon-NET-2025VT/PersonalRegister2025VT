88fa3fd00fc49999cc58cb8f31f584e41f95e046
Jonathan Krall <jonathan.krall@lexicon.se>
2025-04-02 13:44:15 +0200

    Added Payroll

---
+++ PersonalRegister2025VT/Payroll.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalRegister2025VT
+{
+    class Payroll
+    {
+        private List<Employee> employees;
+        
+        public Payroll()
+        {
+            employees = new List<Employee>(); 
+        }
+
+        internal void AddEmployee(string name, int salary)
+        {
+            employees.Add(new Employee(name, salary)); 
+        }
+
+        internal IEnumerable<Employee> GetEmployees()
+        {
+            return employees.ToArray();
+        }
+    }
+}
--- PersonalRegister2025VT/Program.cs
+++ PersonalRegister2025VT/Program.cs
@@ -4,15 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Employee emp = new Employee("Anders", 20000);
-            //emp.Name = "Anders";
-            //emp.Salary = 20000;
+            //Employee emp = new Employee("Anders", 20000);
+            ////emp.Name = "Anders";
+            ////emp.Salary = 20000;
 
-            Console.WriteLine($"Name: {emp.Name} Salary: {emp.Salary}");
-            //Console.WriteLine("Name: " + emp.Name + " Salary: " + emp.Salary);
-            //Console.WriteLine("Name: {0} Salary: {1}", emp.Name, emp.Salary);
-            emp.Name = "Nisse";
-            Console.WriteLine($"Name: {emp.Name} Salary: {emp.Salary}");
+            //Console.WriteLine($"Name: {emp.Name} Salary: {emp.Salary}");
+            ////Console.WriteLine("Name: " + emp.Name + " Salary: " + emp.Salary);
+            ////Console.WriteLine("Name: {0} Salary: {1}", emp.Name, emp.Salary);
+            //emp.Name = "Nisse";
+            //Console.WriteLine($"Name: {emp.Name} Salary: {emp.Salary}");
+
+            Payroll payroll = new Payroll();
+
+            //Lägg till ny anställd
+            payroll.AddEmployee("Kalle", 20000);
+            payroll.AddEmployee("Anna", 25000);
+            //payroll.AddEmployee("Anna2", 25000);
+            //payroll.AddEmployee("Anna3", 25000);
+            //payroll.AddEmployee("Anna4", 25000);
+            //payroll.AddEmployee("Anna5", 25000);
+            //payroll.AddEmployee("Anna6", 25000);
+            //payroll.AddEmployee("Anna7", 25000);
+            //payroll.AddEmployee("Anna8", 25000);
+
+            //Hämtar en samling av alla anställda
+            IEnumerable<Employee> employees = payroll.GetEmployees();
+
+            //Skriver ut alla anställda
+           foreach(Employee employee in employees)
+            {
+                Console.WriteLine($"Name: {employee.Name} Salary: {employee.Salary}");
+            }
 
         }
     }
