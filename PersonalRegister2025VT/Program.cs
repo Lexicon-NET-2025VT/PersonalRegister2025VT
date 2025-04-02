2a5252536a0dafef975fce3150ac2ccbe7cd66ef
Jonathan Krall <jonathan.krall@lexicon.se>
2025-04-02 11:20:55 +0200

    Add project files.

---
+++ PersonalRegister2025VT/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PersonalRegister2025VT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
