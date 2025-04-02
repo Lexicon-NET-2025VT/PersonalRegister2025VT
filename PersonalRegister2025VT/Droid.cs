252636efc942c0dc86d3c9e1137e63a646b60c5d
Jonathan Krall <jonathan.krall@lexicon.se>
2025-04-02 11:47:49 +0200

    Added Droid with Fields and Properties

---
+++ PersonalRegister2025VT/Droid.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalRegister2025VT
+{
+    class Droid
+    {
+        //Field, privat kommer inte åt utanför klassen. Liten bokstav
+        private string firstname;
+
+        private int salary;
+
+        //Property, publik går att komma åt utanför klassen. Stor bokstav
+        public int Salary
+        {
+            get { return salary; }
+            set
+            {
+                if(value < 0)
+                {
+                    salary = 0;
+                }
+                else
+                {
+                    salary = value;
+                }
+                    
+            }
+        }
+        //Autoproperty 
+        public int Salary2 { get; set; }
+
+        //private int salary2;
+
+        //public int Salary2
+        //{
+        //    get { return salary2; }
+        //    set { salary2 = value; }
+        //}
+        
+        public Droid(string name)
+        {
+            firstname = name; 
+        }
+
+        public string GetFirstName()
+        {
+            return firstname; 
+        }
+
+        public void SetFirstName(string name)
+        {
+            firstname = name; 
+        }
+    }
+}
--- PersonalRegister2025VT/Program.cs
+++ PersonalRegister2025VT/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Droid d1 = new Droid("C-3PO");
+            Console.WriteLine(d1.GetFirstName());
+            //d1.firstname = "R2-D2";
+            d1.SetFirstName("R2-D2");
+            Console.WriteLine(d1.GetFirstName());
+            d1.Salary = 25000;
+            Console.WriteLine(d1.Salary);
+            Console.ReadLine(); 
         }
     }
 }
